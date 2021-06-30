d2cb81794c6870b513c062f16417a98e013d836f
SzyCN <52116573+SzyCN@users.noreply.github.com>
2021-07-01 07:36:55 +0800

    Update daoban.js

--- QuantumultX/File/daoban.js
+++ QuantumultX/File/daoban.js
@@ -6,7 +6,6 @@ https:\/\/open\.daobanzhushou\.cn\/shiftassistant\/rs\/user\/lidunka url script-
 var obj = JSON.parse($response.body);
 obj ="remainDays": 450
      "remainMonths": 15
-     "endDate': "2022-09-23"
-     
+    
 $done
 ({body})
